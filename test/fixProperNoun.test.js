d7c13c6c8e1f4d78ccdd00695f0d3a9f53d4b8ab
Martin Grossmann <mhgrossmann@yahoo.com>
2016-03-28 16:10:11 -0500

    added comments to fixProperNoun and created tests

---
+++ test/fixProperNoun.test.js
@@ -0,0 +1,32 @@
+var expect = require('chai').expect;
+var functions = require('../functions.js');
+
+describe('fixProperNoun', function() {
+
+it('should Exist', function() {
+	expect(functions.fixProperNoun).not.to.be.undefined;
+});
+
+it('should throw an error if a string is not entered', function() {
+	expect(function() {functions.fixProperNoun(36)}).to.throw();
+});
+
+it('should return a noun that begins with a capital letter, followed by small letters', function() {
+	expect(functions.fixProperNoun('wAteRmEloN')).to.equal('Watermelon');
+});
+
+it('should return a noun as it is if it already has first letter capitalized and the rest lower case', function() {
+	expect(functions.fixProperNoun('Caterpillar')).to.equal('Caterpillar');
+})
+
+
+});
+
+
+
+
+
+
+
+
+
