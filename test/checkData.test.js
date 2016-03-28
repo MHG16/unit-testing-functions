60fa63a48d04d68b1767a84933e5b93a01a53818
Martin Grossmann <mhgrossmann@yahoo.com>
2016-03-28 13:10:38 -0500

    created tests for checkData

--- test/checkData.test.js
+++ test/checkData.test.js
@@ -5,4 +5,19 @@ describe('checkData', function() {
 	it('should exist', function() {
 		expect(functions.checkData).not.to.be.undefined;
 	});
+
+	it('should return true if string has length of 3', function() {
+		expect(checkData('abc')).to.equal('true');
+	});
+
+	it('should return false if string is longer 3', function() {
+		expect(checkData('happy birthday!')).to.equal('false');
+	});
+
+	it('should return false if string is shorter than 3', function() {
+		expect(checkData('ab')).to.equal('false');
+	});
+
+
+
 });
