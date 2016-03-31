b5bc27ca0e628a2c2790bb489fb0a0e3bf1ec5d3
Martin Grossmann <mhgrossmann@yahoo.com>
2016-03-31 12:36:19 -0500

    created tests for getMonth function

---
+++ test/getMonth.test.js
@@ -0,0 +1,22 @@
+var expect = require('chai').expect;
+var functions = require('../functions.js');
+
+describe('getMonth', function() {
+
+it('should Exist', function() {
+	expect(functions.getMonth).not.to.be.undefined;
+});
+
+it('should throw an error if an integer is not entered', function() {
+	expect(function() {functions.getMonth('a')}).to.throw();
+});
+
+it('should throw an error if an integer between 1 and 12 is not entered', function() {
+	expect(function() {functions.getMonth(14)}).to.throw();
+});
+
+it('should return the corresponding month', function() {
+	expect(functions.getMonth(7)).to.equal('July');
+});
+
+});
