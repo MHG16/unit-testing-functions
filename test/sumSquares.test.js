6a9aa1c54f6f040c6a453d2c8acb59f58bc19c6b
Martin Grossmann <mhgrossmann@yahoo.com>
2016-04-01 14:55:03 -0500

    created tests for sumSquares function

---
+++ test/sumSquares.test.js
@@ -0,0 +1,16 @@
+var expect = require('chai').expect;
+var functions = require('../functions.js');
+
+describe('sumSquares', function() {
+
+it('should Exist', function() {
+	expect(functions.sumSquares).not.to.be.undefined;
+});
+
+it('should throw an error if integer entered is negative or zero', function() {
+	expect(function() {functions.sumSquares('-23', 0)}).to.throw();
+});
+
+it('should return the sum of the squares of all integers from 1 up to an including the number', function() {
+	expect(functions.sumSquares(4)).to.equal(30);
+});
